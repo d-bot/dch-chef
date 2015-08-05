ca87746e2395374575189968e74a12319d5fa41e
dch <dch@dch.io>
2015-08-05 17:15:05 +0000

    update

--- cookbooks/dch/recipes/default.rb
+++ cookbooks/dch/recipes/default.rb
@@ -7,8 +7,18 @@
 # All rights reserved - Do Not Redistribute
 #
 
-%w/ nginx vim /.each do |pkg|
+%w/ vim /.each do |pkg|
   package pkg do
     action :install
+  end
+end
+
+%w/ hosts.allow hosts.deny /.each do |h_conf|
+  file "/etc/#{h_conf}" do
+    owner "root"
+    group "root"
+    mode "0644"
+    content ::File.open("/home/dchoi/chef-repo/cookbooks/dch/files/default/#{h_conf}").read
+    action :create
   end
 end
